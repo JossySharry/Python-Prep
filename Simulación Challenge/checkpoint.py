b9957601aa60abffc3f08dfc864e408176a967f2
Jossy <99308547+JossySharry@users.noreply.github.com>
2024-03-27 13:26:34 -0500

    checkpoint commit

--- Simulación Challenge/checkpoint.py
+++ Simulación Challenge/checkpoint.py
@@ -12,7 +12,16 @@ def Factorial(numero):
         Factorial(-2) debe retornar nulo
     '''
     #Tu código aca:
-    return 'Funcion incompleta'
+    if type(numero) != int:
+        return('Ingrese un número entero')
+    if numero <0:
+        return print('nulo')
+    if numero <=1:
+        return 1
+    factorial_num = numero * Factorial(numero-1)
+    
+    return factorial_num
+
 
 def EsPrimo(valor):
     '''
@@ -26,7 +35,15 @@ def EsPrimo(valor):
         EsPrimo(8) debe retornar False
     '''
     #Tu código aca:
-    return 'Funcion incompleta'
+    if type(valor) != int:
+        return None
+    primo = True
+    for i in range(2, valor):
+        if valor % i == 0:
+            primo = False
+            break
+      
+    return primo
     
 def ClaseAnimal(especie, color):
     '''
@@ -47,4 +64,13 @@ def ClaseAnimal(especie, color):
         a.CumpliAnios() -> debe devolver 3
     '''
     #Tu código aca:
-    return 'Funcion incompleta'
+    class Animal:
+        def __init__(self, especie, color):
+            especie = self.especie
+            color = self.color
+            edad = 0
+        def CumpliAnios(self):
+            self.edad = self.edad+1
+            return self.edad
+
+    return Animal(especie,color)
